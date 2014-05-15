efebbce9ae68c08d54b4326de528086a0f5b9cd8
Dillon Beliveau <dillonbeliveau@gmail.com>
2014-05-15 19:45:20 -0400

    Intial commit

---
+++ cdcsignin.js
@@ -0,0 +1,72 @@
+function processForm()
+{
+    var has_errors = false;
+
+    var netid = $("#inputNetid");
+    var firstname = $("#inputFirstName");
+    var lastname = $("#inputLastName");
+    var phonenumber = $("#inputContactPhone");
+    var email = $("#inputEmail");
+    var description = $("#inputDescription");
+
+    var requiredFields = ["inputFirstName","inputLastName","inputContactPhone","inputEmail","inputDescription"];
+    var errorFields = []
+    for (var i = 0; i < requiredFields.length; i++)
+    {
+        response = processRequiredField(requiredFields[i]);
+        if (response)
+        {
+            has_errors = true;
+            errorFields.push(response);
+        }
+    }
+    if (has_errors)
+    {
+        $(".formerrors").text("Please ensure that all required fields are filled out.");
+        $(".formerrors").css("display","block");
+    }
+    else
+    {
+        var formData = $(".signinform").serialize();
+        $.ajax({
+            type:"POST",
+            url:"http://www.uvm.edu/~dbelivea/other/process_signin.php",
+            data:formData,
+            success: function(data)
+                    {
+                        alert("Submitted.");
+                    }
+        });
+        $("#modalSubmitSuccess").modal();
+    }
+}
+function processRequiredField(field)
+{
+    errors = false;
+    f = $("#"+field);
+    if (!f.val())
+    {
+        errors = true;
+        f.parent().parent().addClass("has-error");
+    }
+    else
+    {
+        f.parent().parent().removeClass("has-error");
+        errors = false;
+    }
+    // Do special error checking for certain fields
+    // Phone number (regex?)
+    // Email address (regex.)
+
+    // Finally, return the name of the failed field.
+    if (errors)
+    {
+        // Get the textual name of the failed field
+        return f.parent().parent().first().first().text();
+    }
+}
+function closeModal()
+{
+    $("#modalSubmitSuccess").modal("toggle");
+    $(".signinform").trigger("reset");
+}
